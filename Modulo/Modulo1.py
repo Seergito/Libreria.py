be45ca095dd358870562d83921903c086df38a03
Seergito <seergitoxno@gmail.com>
2023-03-03 13:07:15 +0100

    modulo

--- Modulo/Modulo1.py
+++ Modulo/Modulo1.py
@@ -1,4 +1,4 @@
-from odoo import models,fields
+from odoo import models,fields,api
 
 class Socio(models.Model):
     _name='Socio'
@@ -24,15 +24,24 @@ class Libros(models.Model):
     autor=fields.Char('Autor del libro')
     editorial=fields.Char('Editorial del libro')
     fechacompra=fields.Date('Fecha de compra') #?
-    preciocompra=fields.Monetary('Precio de compra')
+    preciocompra=fields.Float('Precio de compra')
     categoria=fields.Selection([('intriga','intriga'),('drama','drama'),('infantil','infantil'),('aventuras','aventuras'),('historia','historia')],'Categoria')
-    dniautor=fields.One2many('Autor','dni','DNI Autor') #IMPORTANTE
+    dniautor=fields.Many2many('Autor','Dni autor') #IMPORTANTE
+    antiguedad=fields.Integer('Antiguedad',compute='calcular_antiguedad')
 
+    @api.depends('fechacompra')
+    def calcular_antiguedad(self):
+        self.antiguedad=fields.Date.today().year-self.fechacompra.year
+    
 class Prestamos(models.Model):
     _name='Prestamos'
     codigo=fields.Integer('Codigo prestamo')
     fecha=fields.Datetime('Fecha y Hora del prestamo') 
     numsocio=fields.One2many('Socio','numsocio','Prestamo a socio')
     codigolibro=fields.One2many('Libros','codigo','Codigo libro prestamo')
-    
+    diasprestado=fields.Integer('Dias prestado',compute='calcular_prestamo')
+
+    @api.depends('fecha')
+    def calcular_prestamo(self):
+        self.diasprestado=fields.Date().today()-self.fecha
     
---
+++ Modulo/__manifest_.py
@@ -0,0 +1,7 @@
+{
+'name':'Modulo 1',
+'description':'Gestion Biblioteca',
+'author':'Usuario',
+'depends':['base'],
+'application':True,
+}
