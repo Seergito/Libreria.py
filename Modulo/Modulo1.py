6f311d082eda7392a92a768c9d128d0dbde5b9cf
Seergito <seergitoxno@gmail.com>
2023-03-03 13:31:41 +0100

    PERFECTO

--- Modulo/Modulo1.py
+++ Modulo/Modulo1.py
@@ -1,7 +1,7 @@
 from odoo import models,fields,api
 
 class Socio(models.Model):
-    _name='Socio'
+    _name='socio'
     numsocio=fields.Integer('Numero de socio')
     nombre=fields.Char('Nombre de socio')
     apellidos=fields.Char('Apellido de socio')
@@ -11,14 +11,15 @@ class Socio(models.Model):
     poblacion=fields.Char('Poblacion socio')
     
 class Autor(models.Model):
-    _name='Autor'
+    _name='autor'
     dni=fields.Char('DNI autor')   
     nombre=fields.Char('Nombre autor')
     apellidos=fields.Char('Apellidos autor')
     direccion=fields.Char('Direccion autor')
 
+
 class Libros(models.Model):
-    _name='Libros'
+    _name='libros'
     codigo=fields.Integer('Codigo de libro')
     titulo=fields.Char('Titulo de libro')
     autor=fields.Char('Autor del libro')
@@ -26,7 +27,7 @@ class Libros(models.Model):
     fechacompra=fields.Date('Fecha de compra') #?
     preciocompra=fields.Float('Precio de compra')
     categoria=fields.Selection([('intriga','intriga'),('drama','drama'),('infantil','infantil'),('aventuras','aventuras'),('historia','historia')],'Categoria')
-    dniautor=fields.Many2many('Autor','Dni autor') #IMPORTANTE
+    dniautor=fields.Many2many('autor','Dniautor') #IMPORTANTE
     antiguedad=fields.Integer('Antiguedad',compute='calcular_antiguedad')
 
     @api.depends('fechacompra')
@@ -34,14 +35,13 @@ class Libros(models.Model):
         self.antiguedad=fields.Date.today().year-self.fechacompra.year
     
 class Prestamos(models.Model):
-    _name='Prestamos'
+    _name='prestamos'
     codigo=fields.Integer('Codigo prestamo')
     fecha=fields.Datetime('Fecha y Hora del prestamo') 
-    numsocio=fields.One2many('Socio','numsocio','Prestamo a socio')
-    codigolibro=fields.One2many('Libros','codigo','Codigo libro prestamo')
+    numsocio=fields.One2many('socio','numsocio','Prestamo a socio')
+    codigolibro=fields.One2many('libros','codigo','Codigo libro prestamo')
     diasprestado=fields.Integer('Dias prestado',compute='calcular_prestamo')
 
     @api.depends('fecha')
     def calcular_prestamo(self):
         self.diasprestado=fields.Date().today()-self.fecha
-    
